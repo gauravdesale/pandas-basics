313dff5ee36aa15aeb95ebd879e9b8a6be279459
gauravdesale <desalegaurav60@gmail.com>
2017-09-27 18:50:18 -0500

    this is the challenge for week 4 without using pandas at all

---
+++ titanicChallenge.py
@@ -0,0 +1,31 @@
+import csv
+
+# Read the data
+row_count = sum(1 for row in fileObject)
+specificRate[raw_count]
+with open('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv') as f:
+    csv_reader = csv.reader(f)
+    my_csv = list('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv')
+    survivalRate = my_csv['survived']
+for i in survivalRate:
+    if survivalRate == 1:
+        specificRate[i] += "The survival rate for passenger: " + i " is 1"
+    else if survivalRate == 2
+        specificRate[i] += "The survival rate of passenger " + i + " is: 2"
+    else if survivalRate += 3
+        specificRate[i] += "The survival rate of passenger " + "is: 3"
+        #challenge number 1
+    fee = my_csv['fare']
+for i in fee:
+    total += fee[i]
+average = total / fee.length
+#challenge number 2
+    age = my_csv['age']
+    embarked = m_csv['embarked']
+for i in age:
+    if embarked[i] == "Cherbourg"
+        
+
+    
+
+        
